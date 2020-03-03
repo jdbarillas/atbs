98019bac5b20535e565ce77298bb9c564af78f7c
JDB <jbarillas87@gmail.com>
2020-03-03 04:42:31 +0000

    source_python needs prompt in input to have values, throws error otherwise

--- py/hello.py
+++ py/hello.py
@@ -1,10 +1,10 @@
 # This program says hello and asks for my name
 print('Hello, world!')
 print('What is your name?') # ask for their name
-my_name = input()
+my_name = input('What is your name? ')
 print('It is good to meet you, ' + my_name)
 print('The length of your name is:')
 print(len(my_name))
 print('What is your age?') # ask for their age
-my_age = input()
+my_age = input('What is your age? ')
 print('You will be ' + str(int(my_age)+1) + ' in a year.')
